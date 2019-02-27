99df08d2b9da70d2445bca1dec566af1e9fa37be
frank.hassanabad@elastic.co <frank.hassanabad@elastic.co>
2019-02-27 14:22:25 -0700

    Updated to new major versions and prepped for pushing this into npm

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'suricata-sid-db/data/*.json' {
+  const value: Readonly<Record<string, string>>;
+  export default value;
+}
---
+++ src/sample.ts
@@ -0,0 +1,4 @@
+import * as db from '../data/suricata-rules-ref.json';
+
+// This is a sanity check of logging out a simple number
+console.log(db['2008627']);
